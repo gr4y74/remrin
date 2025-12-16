6a1bcc03c524be97ec7b2a80465a3d7f9af20d02
gr4y74 <gr4y74@gmail.com>
2025-12-16 03:18:04 +0200

    feat: Update UI fonts to Crimson Pro across the application and adjust navigation element font sizes.

--- chat/app/[locale]/layout.tsx
+++ chat/app/[locale]/layout.tsx
@@ -6,12 +6,12 @@ import initTranslations from "@/lib/i18n"
 import { Database } from "@/supabase/types"
 import { createServerClient } from "@supabase/ssr"
 import { Metadata, Viewport } from "next"
-import { Inter } from "next/font/google"
+import { Crimson_Pro } from "next/font/google"
 import { cookies } from "next/headers"
 import { ReactNode } from "react"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const crimson = Crimson_Pro({ subsets: ["latin"] })
 const APP_NAME = "Chatbot UI"
 const APP_DEFAULT_TITLE = "Chatbot UI"
 const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
@@ -88,7 +88,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={crimson.className}>
         <Providers attribute="class" defaultTheme="dark">
           <TranslationsProvider
             namespaces={i18nNamespaces}
