49bddcbae515a142621af2fe0233984a85d809c0
gr4y74 <gr4y74@gmail.com>
2025-12-16 19:40:30 +0200

    Move old remrin content to backup folder

---
+++ _backup_old_remrin/chat/.next/server/_rsc_public_locales_en_translation_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_rsc_public_locales_en_translation_json";
+exports.ids = ["_rsc_public_locales_en_translation_json"];
+exports.modules = {
+
+/***/ "(rsc)/./public/locales/en/translation.json":
+/*!********************************************!*\
+  !*** ./public/locales/en/translation.json ***!
+  \********************************************/
+/***/ ((module) => {
+
+module.exports = JSON.parse('{"Ask anything. Type \\"/\\" for prompts, \\"@\\" for files, and \\"#\\" for tools.":"Ask anything. Type \\"/\\" for prompts, \\"@\\" for files, and \\"#\\" for tools."}');
+
+/***/ })
+
+};
+;
