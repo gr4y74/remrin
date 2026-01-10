0eedc979a441b5ace5d600b2e3c1d9b476f3154b
gr4y74 <gr4y74@gmail.com>
2026-01-10 16:01:15 +0200

    sadsad

--- chat/app/profile/[userId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function ProfileRedirect({ params }: { params: { userId: string } }) {
-    // Redirect to default locale (en) profile page
-    redirect(`/en/profile/${params.userId}`)
-}
